b13d459579fe9ab64303fdb49ff4bd91ed4003fd
JamilRez53 <89921897+JamilRez53@users.noreply.github.com>
2024-11-05 10:45:51 +0600

    ....

---
+++ components/Progressbar.jsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function Progressbar() {
+  return (
+    <View>
+      <Text>Progressbar</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})

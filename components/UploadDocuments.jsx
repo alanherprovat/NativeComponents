1338f79cd5306f91336eb04087a0cf57fe00c666
JamilRez53 <89921897+JamilRez53@users.noreply.github.com>
2024-11-07 17:59:14 +0600

    .............

--- components/UploadDocuments.jsx
+++ components/UploadDocuments.jsx
@@ -1,12 +1,29 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { DocumentIcon } from '../assets/icons/icons'
+import { TouchableOpacity } from 'react-native'
 
-export default function UploadDocuments() {
+export default function UploadDocuments(props) {
+    const {documentName} = props;
   return (
-    <View>
-      <Text>UploadDocuments</Text>
-    </View>
+    <TouchableOpacity style={styles.documentContainer}>
+        <DocumentIcon/>
+        <Text>{documentName}</Text>
+      </TouchableOpacity>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    documentContainer:{
+        width:"90%",
+        gap:10,
+        flexDirection:"row",
+        paddingHorizontal:10,
+        paddingVertical:20,
+        backgroundColor:"#FFFFFFD9",
+        borderColor:"#11AF22",
+        borderWidth:1,
+        alignItems:"center",
+        borderRadius:8
+    }
+})
